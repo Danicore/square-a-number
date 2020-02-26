e64684cc091e968a39455421a26a5ef911928ea8
dani <groddpapyrus@yahoo.com>
2020-02-26 11:41:59 +0500

    number square

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SquareANumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number: ");
+            string str = Console.ReadLine();
+            int a = Convert.ToInt32(str);
+            Console.WriteLine(a * a);
+            Console.Read();
+        }
+    }
+}
